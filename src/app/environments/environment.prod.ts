cc4d6f98ecf7ad8bb0ff1bb6af8a565bb3a98e7a
alfredo_s <alfredo.sirait@photon.com>
2023-05-05 11:54:38 +0700

    move folder environments

--- src/app/environments/environment.prod.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  production: true,
-  name: 'production',
-  baseUrl: 'http://localhost:4200',
-  DOMAIN_URL: 'https://todo.api.devcode.gethired.id',
-};
--- src/app/environments/environment.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  production: false,
-  name: 'dev',
-  baseUrl: 'https://github.com/edosirait/todo-list',
-  DOMAIN_URL: 'https://todo.api.devcode.gethired.id',
-};
